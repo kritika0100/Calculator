5a2bb9be33c6521b8b8efe771323ada5e8677c82
kritika0100 <np03cy4s250022@heraldcollege.edu.np>
2026-03-31 13:49:39 +0545

    Update calc.py
    merged all the branches and imported functions

--- calc.py
+++ calc.py
@@ -1,1 +1,19 @@
+from add import add
+from subtract import subtract
+from multiply import multiply
+from divide import divide
 
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Choose operation (add, subtract, multiply, divide): ")
+
+if operation == "add":
+    print("Result:", add(num1, num2))
+elif operation == "subtract":
+    print("Result:", subtract(num1, num2))
+elif operation == "multiply":
+    print("Result:", multiply(num1, num2))
+elif operation == "divide":
+    print("Result:", divide(num1, num2))
+else:
+    print("Invalid operation")
